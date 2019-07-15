93cda03e9bdf17b056f9f4fd1043a9f43e4ab2a1
Wayne Lai <waynelai614@gmail.com>
2019-07-15 23:36:43 +0800

    Remove unused files

--- scripts/node_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit -o pipefail -o nounset
-
-echo "Updating npm dependencies."
-npm install -g npm
-npm install -g elm elm-live elm-format gulp gulp-elm
-npm install -g typescript gulp-typescript tern
-npm install -g ios-deploy
--- scripts/system_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit -o pipefail -o nounset
-
-echo "Disabling netbiosd"
-set -x
-sudo launchctl disable system/netbiosd
